d8a61c1143fdbaed9034eeed6842186e875bd801
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-05-20 19:11:32 -0300

    Added method do transfer energy

--- src/main/kotlin/me/steven/indrev/blocks/ElectricBlock.kt
+++ src/main/kotlin/me/steven/indrev/blocks/ElectricBlock.kt
@@ -1,6 +1,5 @@
 package me.steven.indrev.blocks
 
-import me.steven.indrev.blocks.generators.GeneratorBlock
 import net.minecraft.block.Block
 import net.minecraft.block.BlockEntityProvider
 import net.minecraft.block.BlockState
@@ -9,8 +8,11 @@ import net.minecraft.item.ItemPlacementContext
 import net.minecraft.state.StateManager
 import net.minecraft.state.property.DirectionProperty
 import net.minecraft.state.property.Properties
-import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
 import net.minecraft.world.BlockView
+import net.minecraft.world.World
+import team.reborn.energy.Energy
+import kotlin.math.max
 
 open class ElectricBlock(settings: Settings, open val maxBuffer: Double, open val blockEntityProvider: () -> ElectricBlockEntity) : Block(settings), BlockEntityProvider {
     override fun createBlockEntity(view: BlockView?): BlockEntity? = blockEntityProvider()
@@ -21,6 +23,17 @@ open class ElectricBlock(settings: Settings, open val maxBuffer: Double, open va
 
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>?) {
         builder?.add(FACING)
+    }
+
+    fun tryProvideEnergyTo(world: World, sourcePos: BlockPos, targetPos: BlockPos): Double {
+        val sourceBlockEntity = world.getBlockEntity(sourcePos)
+        if (sourceBlockEntity !is ElectricBlockEntity) return 0.0
+        val targetBlockEntity = world.getBlockEntity(targetPos)
+        if (targetBlockEntity !is ElectricBlockEntity) return 0.0
+        val sourceHandler = Energy.of(sourceBlockEntity)
+        val targetHandler = Energy.of(targetBlockEntity)
+        val amount = if (sourceHandler.maxOutput > targetHandler.maxInput) targetHandler.maxInput else sourceHandler.maxOutput
+        return sourceHandler.into(targetHandler).move(amount)
     }
 
     companion object {
