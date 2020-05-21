5cc3fa1cd283dc8ce60fbb1b92cbe614f918881f
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-05-21 12:01:33 -0300

    Improved EnergyWidget.kt

--- src/main/kotlin/me/steven/indrev/gui/widgets/EnergyWidget.kt
+++ src/main/kotlin/me/steven/indrev/gui/widgets/EnergyWidget.kt
@@ -5,29 +5,20 @@ import io.github.cottonmc.cotton.gui.widget.WWidget
 import me.steven.indrev.identifier
 import net.minecraft.client.resource.language.I18n
 import net.minecraft.container.PropertyDelegate
-import java.awt.Color
-import kotlin.math.absoluteValue
-import kotlin.math.ceil
+import kotlin.math.round
 
 class EnergyWidget(private val delegate: PropertyDelegate) : WWidget() {
     init {
         this.setSize(16, 64)
     }
-    private var lastHeightUpdate = 0
-    private var lastVUpdate = 0f
     override fun paintBackground(x: Int, y: Int) {
         ScreenDrawing.texturedRect(x, y, width, height, ENERGY_EMPTY, -1)
         val energy = delegate[0]
         val maxEnergy = delegate[1]
         if (energy > 0) {
-            val v = energy.toFloat() / maxEnergy
-            val h = ceil(v * height).toInt()
-            val diff = h - lastHeightUpdate
-            if (diff > 1) {
-                lastHeightUpdate = h
-                lastVUpdate = 1f - v
-            }
-            ScreenDrawing.texturedRect(x, y + (height - lastHeightUpdate), width, lastHeightUpdate, ENERGY_FULL, 0f, lastVUpdate, 1f, 1f, -1)
+            val v = ((energy.toFloat() * 63 / maxEnergy) + 1) / 64
+            val h = round(v * height).toInt()
+            ScreenDrawing.texturedRect(x, y + (height - h), width, h, ENERGY_FULL, 0f, 1f-v, 1f, 1f, -1)
         }
     }
 
