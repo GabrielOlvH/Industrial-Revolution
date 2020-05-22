33ba85d0cb812c5bf37304cfce1904b0f9a8f045
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-05-22 20:23:57 -0300

    Moved ore generation utility method

--- src/main/kotlin/me/steven/indrev/content/ItemRegistry.kt
+++ src/main/kotlin/me/steven/indrev/content/ItemRegistry.kt
@@ -1,12 +1,19 @@
 package me.steven.indrev.content
 
 import me.steven.indrev.IndustrialRevolution
+import me.steven.indrev.block
 import me.steven.indrev.identifier
 import me.steven.indrev.item
 import me.steven.indrev.items.Upgrade
 import me.steven.indrev.items.UpgradeItem
-import me.steven.indrev.ore
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags
+import net.minecraft.block.Block
+import net.minecraft.block.Material
+import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
+import net.minecraft.sound.BlockSoundGroup
+import net.minecraft.util.Identifier
 
 class ItemRegistry {
 
@@ -34,5 +41,13 @@ class ItemRegistry {
         val BUFFER_UPGRADE = UpgradeItem(Item.Settings().group(IndustrialRevolution.MOD_GROUP).maxCount(1), Upgrade.BUFFER)
         val SPEED_UPGRADE = UpgradeItem(Item.Settings().group(IndustrialRevolution.MOD_GROUP).maxCount(1), Upgrade.SPEED)
         val ENERGY_UPGRADE = UpgradeItem(Item.Settings().group(IndustrialRevolution.MOD_GROUP).maxCount(1), Upgrade.ENERGY)
+
+        private fun Identifier.ore(pulverizable: Boolean): Block {
+            val key = this.path
+            val block = Block(FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES))
+            identifier("${key}_ore").block(block).item(BlockItem(block, Item.Settings().group(IndustrialRevolution.MOD_GROUP)))
+            if (pulverizable) identifier("pulverized_$key").item(Item(Item.Settings().group(IndustrialRevolution.MOD_GROUP)))
+            return block
+        }
     }
 }
