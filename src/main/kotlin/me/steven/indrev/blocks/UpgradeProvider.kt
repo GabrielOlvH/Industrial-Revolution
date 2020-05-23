a59ad1ea0050357d8a6d2e491385c622071479b4
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-05-23 20:47:38 -0300

    Removed unused method

--- src/main/kotlin/me/steven/indrev/blocks/UpgradeProvider.kt
+++ src/main/kotlin/me/steven/indrev/blocks/UpgradeProvider.kt
@@ -1,9 +1,6 @@
 package me.steven.indrev.blocks
 
 import me.steven.indrev.items.Upgrade
-import me.steven.indrev.items.UpgradeItem
-import net.minecraft.inventory.Inventory
-import net.minecraft.item.ItemStack
 
 interface UpgradeProvider {
 
@@ -12,13 +9,4 @@ interface UpgradeProvider {
     fun getAvailableUpgrades(): Array<Upgrade>
 
     fun getBaseValue(upgrade: Upgrade): Double
-
-    fun getUpgrades(inventory: Inventory): Array<ItemStack> {
-        val upgrades = mutableListOf<ItemStack>()
-        for (i in getUpgradeSlots()) {
-            val invStack = inventory.getInvStack(i)
-            if (invStack.item is UpgradeItem) upgrades.add(invStack)
-        }
-        return upgrades.toTypedArray()
-    }
 }
