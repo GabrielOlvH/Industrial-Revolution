72f0ef300cb962114129e92a076be39fbedb1c9b
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-05-22 22:55:18 -0300

    Avoid rendering air as a cover

--- src/main/kotlin/me/steven/indrev/blocks/cables/CableBlockEntityRenderer.kt
+++ src/main/kotlin/me/steven/indrev/blocks/cables/CableBlockEntityRenderer.kt
@@ -1,5 +1,6 @@
 package me.steven.indrev.blocks.cables
 
+import net.minecraft.block.Blocks
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.render.RenderLayers
 import net.minecraft.client.render.VertexConsumerProvider
@@ -11,8 +12,9 @@ import net.minecraft.util.registry.Registry
 class CableBlockEntityRenderer(dispatcher: BlockEntityRenderDispatcher) : BlockEntityRenderer<CableBlockEntity>(dispatcher) {
     override fun render(blockEntity: CableBlockEntity?, tickDelta: Float, matrices: MatrixStack?, vertexConsumers: VertexConsumerProvider?, light: Int, overlay: Int) {
         if (blockEntity?.cover != null) {
-            matrices?.push()
             val state = Registry.BLOCK.get(blockEntity.cover).defaultState
+            if (state == Blocks.AIR.defaultState) return
+            matrices?.push()
             val vertexConsumer = vertexConsumers?.getBuffer(RenderLayers.getBlockLayer(state)) ?: return
             MinecraftClient.getInstance().blockRenderManager.renderBlock(state, blockEntity.pos, blockEntity.world, matrices, vertexConsumer, true, blockEntity.world?.random)
             matrices?.pop()
