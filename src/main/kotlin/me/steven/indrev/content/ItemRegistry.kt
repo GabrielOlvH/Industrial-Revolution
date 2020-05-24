6252f2dfd925cfa6751a0b2ec29ce6288900a3a1
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-05-24 19:08:43 -0300

    Some extra items and recipes

--- src/main/kotlin/me/steven/indrev/content/ItemRegistry.kt
+++ src/main/kotlin/me/steven/indrev/content/ItemRegistry.kt
@@ -18,18 +18,21 @@ class ItemRegistry {
 
     fun registerAll() {
         identifier("copper_ore").block(COPPER_ORE).item(COPPER_ORE_ITEM)
-        identifier("pulverized_copper").item(PULVERIZED_COPPER)
-        identifier("copper_ingot").item(COPPER_INGOT)
+        identifier("pulverized_copper").item(BASIC_ITEM())
+        identifier("copper_ingot").item(BASIC_ITEM())
         identifier("tin_ore").block(TIN_ORE).item(TIN_ORE_ITEM)
-        identifier("pulverized_tin").item(PULVERIZED_TIN)
-        identifier("tin_ingot").item(TIN_INGOT)
+        identifier("pulverized_tin").item(BASIC_ITEM())
+        identifier("tin_ingot").item(BASIC_ITEM())
         identifier("nikolite_ore").block(NIKOLITE_ORE).item(NIKOLITE_ORE_ITEM)
-        identifier("nikolite").item(NIKOLITE)
+        identifier("nikolite").item(BASIC_ITEM())
 
-        identifier("pulverized_iron").item(PULVERIZED_IRON)
-        identifier("pulverized_gold").item(PULVERIZED_GOLD)
-        identifier("pulverized_coal").item(PULVERIZED_COAL)
-        identifier("pulverized_diamond").item(PULVERIZED_DIAMOND)
+        identifier("pulverized_iron").item(BASIC_ITEM())
+        identifier("pulverized_gold").item(BASIC_ITEM())
+        identifier("pulverized_coal").item(BASIC_ITEM())
+        identifier("pulverized_diamond").item(BASIC_ITEM())
+
+        identifier("iron_plate").item(BASIC_ITEM())
+        identifier("basic_capacitor").item(BASIC_ITEM())
 
         identifier("buffer_upgrade").item(BUFFER_UPGRADE)
         identifier("speed_upgrade").item(SPEED_UPGRADE)
@@ -39,22 +42,14 @@ class ItemRegistry {
     companion object {
         private val ORE_BLOCK_SETTINGS: FabricBlockSettings = FabricBlockSettings.of(Material.STONE).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES)
 
+        private val BASIC_ITEM: () -> Item = { Item(itemSettings()) }
+
         val COPPER_ORE = Block(ORE_BLOCK_SETTINGS)
         val COPPER_ORE_ITEM = BlockItem(COPPER_ORE, itemSettings())
-        val PULVERIZED_COPPER = Item(itemSettings())
-        val COPPER_INGOT = Item(itemSettings())
         val TIN_ORE = Block(ORE_BLOCK_SETTINGS)
         val TIN_ORE_ITEM = BlockItem(TIN_ORE, itemSettings())
-        val PULVERIZED_TIN = Item(itemSettings())
-        val TIN_INGOT = Item(itemSettings())
         val NIKOLITE_ORE = Block(ORE_BLOCK_SETTINGS)
         val NIKOLITE_ORE_ITEM = BlockItem(NIKOLITE_ORE, itemSettings())
-        val NIKOLITE = Item(itemSettings())
-
-        val PULVERIZED_IRON = Item(itemSettings())
-        val PULVERIZED_GOLD = Item(itemSettings())
-        val PULVERIZED_COAL = Item(itemSettings())
-        val PULVERIZED_DIAMOND = Item(itemSettings())
 
         val BUFFER_UPGRADE = UpgradeItem(itemSettings().maxCount(1), Upgrade.BUFFER)
         val SPEED_UPGRADE = UpgradeItem(itemSettings().maxCount(1), Upgrade.SPEED)
