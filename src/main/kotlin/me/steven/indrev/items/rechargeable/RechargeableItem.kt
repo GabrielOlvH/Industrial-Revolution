22b2c005a336e27cf6468980ff040c3fdff87ea5
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-05-25 21:14:17 -0300

    Removed bad hook

--- src/main/kotlin/me/steven/indrev/items/rechargeable/RechargeableItem.kt
+++ src/main/kotlin/me/steven/indrev/items/rechargeable/RechargeableItem.kt
@@ -1,13 +1,5 @@
 package me.steven.indrev.items.rechargeable
 
-import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
-import net.minecraft.item.ItemStack
-import net.minecraft.world.World
 
-class RechargeableItem(settings: Settings) : Item(settings), Rechargeable {
-    override fun onCraft(stack: ItemStack?, world: World?, player: PlayerEntity?) {
-        if (stack == null) return
-        stack.damage = stack.maxDamage - 1
-    }
-}
+class RechargeableItem(settings: Settings) : Item(settings), Rechargeable
