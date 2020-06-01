a1521b94357ea313c33c7e34a36dec5ee7ac0c38
StupPlayer <gabrielhenrique1143@outlook.com>
2020-05-31 22:25:00 -0300

    Added better tooltips for rechargeable items

--- src/main/kotlin/me/steven/indrev/items/rechargeable/RechargeableItem.kt
+++ src/main/kotlin/me/steven/indrev/items/rechargeable/RechargeableItem.kt
@@ -1,5 +1,27 @@
 package me.steven.indrev.items.rechargeable
 
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.text.LiteralText
+import net.minecraft.text.Text
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.Formatting
+import net.minecraft.world.World
 
-class RechargeableItem(settings: Settings, val canOutput: Boolean = false) : Item(settings), Rechargeable
+class RechargeableItem(settings: Settings, val canOutput: Boolean = false) : Item(settings), Rechargeable {
+
+    override fun appendTooltip(
+        stack: ItemStack?,
+        world: World?,
+        tooltip: MutableList<Text>?,
+        context: TooltipContext?
+    ) {
+        super.appendTooltip(stack, world, tooltip, context)
+        tooltip?.add(TranslatableText("gui.widget.energy"))
+        tooltip?.add(LiteralText("${stack?.damage} / ${stack?.maxDamage} LF"))
+        tooltip?.add(TranslatableText("item.indrev.rechargeable.tooltip").formatted(Formatting.ITALIC, Formatting.GRAY))
+    }
+
+    override fun canRepair(stack: ItemStack?, ingredient: ItemStack?): Boolean = false
+}
--- src/main/kotlin/me/steven/indrev/items/rechargeable/RechargeableMiningItem.kt
+++ src/main/kotlin/me/steven/indrev/items/rechargeable/RechargeableMiningItem.kt
@@ -2,14 +2,33 @@ package me.steven.indrev.items.rechargeable
 
 import net.minecraft.block.BlockState
 import net.minecraft.block.Material
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.ItemStack
 import net.minecraft.item.PickaxeItem
 import net.minecraft.item.ToolMaterials
+import net.minecraft.text.BaseText
+import net.minecraft.text.LiteralText
+import net.minecraft.text.Text
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.Formatting
+import net.minecraft.world.World
 
 class RechargeableMiningItem(settings: Settings) : PickaxeItem(ToolMaterials.DIAMOND, 0, 0F, settings), Rechargeable {
     override fun getMiningSpeed(stack: ItemStack?, state: BlockState?): Float {
         val material = state?.material
         return if (SUPPORTED_MATERIALS.contains(material)) 16F else this.material.miningSpeed
+    }
+
+    override fun appendTooltip(
+        stack: ItemStack?,
+        world: World?,
+        tooltip: MutableList<Text>?,
+        context: TooltipContext?
+    ) {
+        super.appendTooltip(stack, world, tooltip, context)
+        tooltip?.add(TranslatableText("gui.widget.energy"))
+        tooltip?.add(LiteralText("${stack?.damage} / ${stack?.maxDamage} LF"))
+        tooltip?.add(TranslatableText("item.indrev.rechargeable.tooltip").formatted(Formatting.ITALIC, Formatting.GRAY))
     }
 
     override fun canRepair(stack: ItemStack?, ingredient: ItemStack?): Boolean = false
