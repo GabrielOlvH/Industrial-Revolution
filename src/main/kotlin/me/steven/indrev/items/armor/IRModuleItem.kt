c1bc51788ec3f5c4fae3a494b130826aad135f3c
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-07-09 12:09:27 -0300

    Added tooltip for modules

--- src/main/kotlin/me/steven/indrev/items/armor/IRModuleItem.kt
+++ src/main/kotlin/me/steven/indrev/items/armor/IRModuleItem.kt
@@ -1,6 +1,16 @@
 package me.steven.indrev.items.armor
 
 import me.steven.indrev.armor.Module
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.Formatting
+import net.minecraft.world.World
 
-class IRModuleItem(val module: Module, settings: Settings) : Item(settings)
+class IRModuleItem(val module: Module, settings: Settings) : Item(settings) {
+    override fun appendTooltip(stack: ItemStack?, world: World?, tooltip: MutableList<Text>?, context: TooltipContext?) {
+        tooltip?.add(TranslatableText("item.indrev.module_${module.key}.tooltip").formatted(Formatting.BLUE, Formatting.ITALIC))
+    }
+}
