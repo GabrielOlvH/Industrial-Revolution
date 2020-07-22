84c8d162342bdaf7387dd50b2a626e7ec013a9ee
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-07-21 21:13:16 -0300

    double rancher cooldown

--- src/main/kotlin/me/steven/indrev/blockentities/farms/RancherBlockEntity.kt
+++ src/main/kotlin/me/steven/indrev/blockentities/farms/RancherBlockEntity.kt
@@ -94,7 +94,7 @@ class RancherBlockEntity(tier: Tier) : AOEMachineBlockEntity(tier, MachineRegist
                 fakePlayer.inventory.setStack(1, ItemStack.EMPTY)
             }
         }
-        cooldown += 6 - (Upgrade.SPEED.apply(this, inventory).toInt() / 4)
+        cooldown += 12 - (Upgrade.SPEED.apply(this, inventory).toInt() / 4)
     }
 
     private fun filterAnimalsToKill(entities: List<AnimalEntity>): List<AnimalEntity> {
