000c1118fbdc9274fb8d8c020cedc104c7591144
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-08-15 14:16:31 -0300

    Nerf Smelter fluid storage to 8 buckets

--- src/main/kotlin/me/steven/indrev/blockentities/crafters/SmelterBlockEntity.kt
+++ src/main/kotlin/me/steven/indrev/blockentities/crafters/SmelterBlockEntity.kt
@@ -30,7 +30,7 @@ class SmelterBlockEntity(tier: Tier) :
                 }
             }
         }
-        this.fluidComponent = FluidComponent(FluidAmount(50))
+        this.fluidComponent = FluidComponent(FluidAmount(8))
     }
 
     private var currentRecipe: SmelterRecipe? = null
