24c248184045323031dfd07fe7d093b4f6f8f0c8
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-08-16 11:51:38 -0300

    Fix pump

--- src/main/kotlin/me/steven/indrev/blockentities/farms/PumpBlockEntity.kt
+++ src/main/kotlin/me/steven/indrev/blockentities/farms/PumpBlockEntity.kt
@@ -1,12 +1,13 @@
 package me.steven.indrev.blockentities.farms
 
-import alexiil.mc.lib.attributes.Simulation
 import alexiil.mc.lib.attributes.fluid.amount.FluidAmount
 import alexiil.mc.lib.attributes.fluid.volume.FluidKeys
 import me.steven.indrev.IndustrialRevolution
 import me.steven.indrev.components.FluidComponent
 import me.steven.indrev.registry.MachineRegistry
 import me.steven.indrev.utils.Tier
+import net.minecraft.block.Blocks
+import net.minecraft.block.FluidBlock
 import net.minecraft.block.FluidDrainable
 import net.minecraft.fluid.Fluids
 import net.minecraft.util.math.Box
@@ -15,11 +16,11 @@ import team.reborn.energy.EnergySide
 class PumpBlockEntity(tier: Tier) : AOEMachineBlockEntity(tier, MachineRegistry.PUMP_REGISTRY) {
 
     init {
-        this.fluidComponent = FluidComponent(FluidAmount(1))
+        this.fluidComponent = FluidComponent(FluidAmount.BUCKET)
     }
 
     override fun machineTick() {
-        if ((world?.time ?: return) % 20 == 0L) return
+        if ((world?.time ?: return) % 20 == 0L || fluidComponent?.volume?.isEmpty == false) return
         val fluidComponent = fluidComponent ?: return
         val down = pos.down()
         val hasFluid = world?.getFluidState(down)?.isEmpty == false
@@ -27,18 +28,19 @@ class PumpBlockEntity(tier: Tier) : AOEMachineBlockEntity(tier, MachineRegistry.
         if (hasFluid) {
             val mutablePos = down.mutableCopy()
             for (x in range.minX.toInt()..range.maxX.toInt())
-                for (z in range.minZ.toInt()..range.maxZ.toInt()) {
-                    mutablePos.set(x, down.y, z)
-                    val fluidState = world?.getFluidState(mutablePos)
-                    val fluidBlock = fluidState?.blockState?.block
-                    if (fluidState?.isEmpty == false) {
-                        val toInsert = FluidKeys.get(fluidState.fluid).withAmount(FluidAmount.BUCKET)
-                        if (fluidComponent.insertable.attemptInsertion(toInsert, Simulation.SIMULATE).isEmpty
-                            && fluidBlock is FluidDrainable
-                            && fluidBlock.tryDrainFluid(world, mutablePos, fluidState.blockState) != Fluids.EMPTY
-                        ) {
-                            fluidComponent.insertable.insert(toInsert)
-                            break
+                for (y in range.minY.toInt()..range.maxY.toInt()) {
+                    for (z in range.minZ.toInt()..range.maxZ.toInt()) {
+                        mutablePos.set(x, y, z)
+                        val blockState = world?.getBlockState(mutablePos)
+                        val block = blockState?.block
+                        if (block is FluidDrainable && block is FluidBlock) {
+                            val drained = block.tryDrainFluid(world, mutablePos, blockState)
+                            if (drained != Fluids.EMPTY) {
+                                val toInsert = FluidKeys.get(drained).withAmount(FluidAmount.BUCKET)
+                                world?.setBlockState(mutablePos, Blocks.AIR.defaultState)
+                                fluidComponent.insertable.insert(toInsert)
+                                break
+                            }
                         }
                     }
                 }
