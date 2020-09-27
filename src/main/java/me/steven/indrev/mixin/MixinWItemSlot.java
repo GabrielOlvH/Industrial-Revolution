4fc183af26d4aba26a8a95630e212093ad14aadc
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-09-27 15:57:35 -0300

    fix slot painters being override

---
+++ src/main/java/me/steven/indrev/mixin/MixinWItemSlot.java
@@ -0,0 +1,21 @@
+package me.steven.indrev.mixin;
+
+import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
+import io.github.cottonmc.cotton.gui.widget.WItemSlot;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WItemSlot.class)
+public class MixinWItemSlot {
+    @Shadow @Nullable
+    private BackgroundPainter backgroundPainter;
+
+    @Inject(method = "addPainters", at = @At("HEAD"), cancellable = true, remap = false)
+    private void indrev_dontOverridePainters(CallbackInfo ci) {
+        if (backgroundPainter != null) ci.cancel();
+    }
+}
