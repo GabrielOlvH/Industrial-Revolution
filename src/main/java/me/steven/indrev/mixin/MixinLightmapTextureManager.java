198dd46e008b6d6985e8dade776848e9c9fa8636
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-10-24 17:12:22 -0300

    fix modular stuff and uh yes

---
+++ src/main/java/me/steven/indrev/mixin/MixinLightmapTextureManager.java
@@ -0,0 +1,28 @@
+package me.steven.indrev.mixin;
+
+import me.steven.indrev.items.armor.IRModularArmor;
+import me.steven.indrev.tools.modular.ArmorModule;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(LightmapTextureManager.class)
+public abstract class MixinLightmapTextureManager {
+
+    @ModifyVariable(
+            method = "update",
+            at = @At(
+                    value = "INVOKE_ASSIGN"
+            ), ordinal = 2)
+    private float a(float amount) {
+        for (ItemStack stack : MinecraftClient.getInstance().player.getArmorItems()) {
+            if (stack.getItem() instanceof IRModularArmor && ArmorModule.NIGHT_VISION.getLevel(stack) > 0) {
+                return 1.0f;
+            }
+        }
+        return amount;
+    }
+}
