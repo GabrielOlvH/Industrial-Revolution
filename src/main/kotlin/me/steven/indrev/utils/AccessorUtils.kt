5777dae9bb1cc3ed767ac4ee08d5ff686b4344d4
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-11-02 14:04:36 -0300

    clear recipe cache at the end of datapack reload

--- src/main/kotlin/me/steven/indrev/utils/AccessorUtils.kt
+++ src/main/kotlin/me/steven/indrev/utils/AccessorUtils.kt
@@ -5,6 +5,7 @@ import me.steven.indrev.recipes.machines.IRRecipe
 import net.minecraft.entity.passive.AnimalEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
+import net.minecraft.recipe.Recipe
 import net.minecraft.recipe.RecipeManager
 import net.minecraft.recipe.RecipeType
 import net.minecraft.screen.Property
@@ -29,6 +30,8 @@ val EnergyHandler.side: EnergySide
 
 fun <T : IRRecipe> RecipeManager.getAllOfType(type: RecipeType<T>): Map<Identifier, T>
         = (this as AccessorRecipeManager).indrev_getAllOfType(type) as Map<Identifier, T>
+
+fun RecipeManager.getRecipes(): Map<RecipeType<*>, Map<Identifier, Recipe<*>>> = (this as AccessorRecipeManager).recipes
 
 val ScreenHandler.properties: List<Property>
     get() = (this as AccessorScreenHandler).properties
