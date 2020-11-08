7a9f83b764a41a6e8078d412234c8d0ffd954740
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-11-08 17:43:59 -0300

    temp stuff

--- src/main/kotlin/me/steven/indrev/IndustrialRevolutionClient.kt
+++ src/main/kotlin/me/steven/indrev/IndustrialRevolutionClient.kt
@@ -9,7 +9,6 @@ import me.steven.indrev.blockentities.modularworkbench.ModularWorkbenchBlockEnti
 import me.steven.indrev.blockentities.storage.ChargePadBlockEntityRenderer
 import me.steven.indrev.blockentities.storage.TankBlockEntityRenderer
 import me.steven.indrev.blocks.CableModel
-import me.steven.indrev.blocks.machine.DrillHeadModel
 import me.steven.indrev.fluids.FluidType
 import me.steven.indrev.gui.IRInventoryScreen
 import me.steven.indrev.gui.IRModularControllerScreen
@@ -29,7 +28,6 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry
-import net.fabricmc.fabric.api.client.model.ModelResourceProvider
 import net.fabricmc.fabric.api.client.model.ModelVariantProvider
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry
@@ -146,13 +144,13 @@ object IndustrialRevolutionClient : ClientModInitializer {
             }
         }
 
-        ModelLoadingRegistry.INSTANCE.registerResourceProvider {
-            ModelResourceProvider { identifier, _ ->
-                if (identifier.namespace == "indrev" && identifier.path == "drill_head")
-                return@ModelResourceProvider DrillHeadModel
-                else return@ModelResourceProvider null
-            }
-        }
+        //ModelLoadingRegistry.INSTANCE.registerResourceProvider {
+            //ModelResourceProvider { identifier, _ ->
+                //if (identifier.namespace == "indrev" && identifier.path == "drill_head")
+                //return@ModelResourceProvider DrillHeadModel
+                //else return@ModelResourceProvider null
+            //}
+        //}
 
         FabricModelPredicateProviderRegistry.register(
             IRRegistry.GAMER_AXE_ITEM,
