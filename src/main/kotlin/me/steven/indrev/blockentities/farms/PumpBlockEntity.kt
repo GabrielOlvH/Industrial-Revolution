56552f229c105161879b24c28a93ccdbe711e462
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-11-21 14:14:49 -0300

    Now it compiles

--- src/main/kotlin/me/steven/indrev/blockentities/farms/PumpBlockEntity.kt
+++ src/main/kotlin/me/steven/indrev/blockentities/farms/PumpBlockEntity.kt
@@ -3,7 +3,7 @@ package me.steven.indrev.blockentities.farms
 import alexiil.mc.lib.attributes.fluid.amount.FluidAmount
 import alexiil.mc.lib.attributes.fluid.volume.FluidKeys
 import me.steven.indrev.blockentities.MachineBlockEntity
-import me.steven.indrev.components.FluidComponent
+import me.steven.indrev.components.fluid.FluidComponent
 import me.steven.indrev.config.BasicMachineConfig
 import me.steven.indrev.registry.MachineRegistry
 import me.steven.indrev.utils.Tier
