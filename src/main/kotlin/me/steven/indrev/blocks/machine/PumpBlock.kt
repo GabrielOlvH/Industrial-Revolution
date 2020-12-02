038d296f2aaed505e50be7239ea17eb48f0e6831
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-12-02 17:55:03 -0300

    pump hitbox

--- src/main/kotlin/me/steven/indrev/blocks/machine/PumpBlock.kt
+++ src/main/kotlin/me/steven/indrev/blocks/machine/PumpBlock.kt
@@ -5,8 +5,11 @@ import me.steven.indrev.blockentities.farms.PumpBlockEntity
 import me.steven.indrev.utils.Tier
 import net.minecraft.block.BlockState
 import net.minecraft.block.ShapeContext
+import net.minecraft.item.ItemPlacementContext
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 
 class PumpBlock(settings: Settings) : HorizontalFacingMachineBlock(
@@ -21,9 +24,22 @@ class PumpBlock(settings: Settings) : HorizontalFacingMachineBlock(
         world: BlockView?,
         pos: BlockPos?,
         context: ShapeContext?
-    ): VoxelShape = SHAPE
+    ): VoxelShape = when (state?.get(HORIZONTAL_FACING)) {
+        Direction.NORTH -> SHAPE_NORTH
+        Direction.SOUTH -> SHAPE_SOUTH
+        Direction.WEST -> SHAPE_WEST
+        Direction.EAST -> SHAPE_EAST
+        else -> VoxelShapes.fullCube()
+    }
+
+    override fun getPlacementState(ctx: ItemPlacementContext?): BlockState? {
+        return this.defaultState.with(HORIZONTAL_FACING, ctx?.playerFacing)
+    }
 
     companion object {
-        private val SHAPE = createCuboidShape(2.0, 0.0, 2.0, 14.0, 16.0, 14.0)
+        private val SHAPE_NORTH = createCuboidShape(2.5, 0.0, 0.0, 14.5, 16.0, 14.5)
+        private val SHAPE_SOUTH = createCuboidShape(2.5, 0.0, 2.5, 14.5, 16.0, 16.0)
+        private val SHAPE_WEST = createCuboidShape(0.0, 0.0, 2.5, 14.5, 16.0, 14.5)
+        private val SHAPE_EAST = createCuboidShape(2.5, 0.0, 2.5, 16.0, 16.0, 14.5)
     }
 }
