c5bc9086b445f1c16755e5e37163ef413ffb2ba9
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-12-17 15:49:44 -0300

    Reimplement side-to-side connections but simpler because i don't want to be fucking bothered to touch that mess

--- src/main/kotlin/me/steven/indrev/energy/IREnergyMovement.kt
+++ src/main/kotlin/me/steven/indrev/energy/IREnergyMovement.kt
@@ -1,7 +1,7 @@
 package me.steven.indrev.energy
 
-import me.steven.indrev.blockentities.cables.CableBlockEntity
-import me.steven.indrev.blocks.machine.CableBlock
+import dev.technici4n.fasttransferlib.api.energy.EnergyApi
+import dev.technici4n.fasttransferlib.api.energy.EnergyMovement
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
@@ -10,40 +10,12 @@ import net.minecraft.util.math.Direction
 @Suppress("CAST_NEVER_SUCCEEDS")
 object IREnergyMovement {
     fun spreadNeighbors(source: BlockEntity, pos: BlockPos) {
-        /*val world = source.world
-        if (source !is EnergyStorage) return
-        val sourceHandler = Energy.of(source)
-        val targets = Direction.values()
-            .mapNotNull { direction ->
-                if (source.getMaxOutput(EnergySide.fromMinecraft(direction)) > 0 && isSideConnected(source, direction)) {
-                    val targetPos = pos.offset(direction)
-                    val target = world?.getBlockEntity(targetPos)
-                    if (target != null && Energy.valid(target) && isSideConnected(target, direction.opposite)) {
-                        val targetHandler = Energy.of(target).side(direction.opposite)
-                        if (targetHandler.energy < targetHandler.maxStored) targetHandler
-                        else null
-                    } else null
-                } else null
+        Direction.values()
+            .forEach { direction ->
+                val sourceIo = EnergyApi.SIDED[source.world, pos, direction]
+                val targetIo = EnergyApi.SIDED[source.world, pos.offset(direction), direction.opposite]
+                if (sourceIo != null && targetIo != null)
+                    EnergyMovement.move(sourceIo, targetIo, Double.MAX_VALUE)
             }
-        val sum = targets.sumByDouble { targetHandler ->
-            (targetHandler.maxStored - targetHandler.energy).coerceAtMost(targetHandler.maxInput)
-        }
-        targets.sortedByDescending { it.energy }.forEach { targetHandler ->
-            val direction = targetHandler.side
-            val target = targetHandler.holder
-            sourceHandler.side(direction.opposite())
-            val targetMaxInput = targetHandler.maxInput
-            val energy = sourceHandler.energy.coerceAtMost(sourceHandler.maxOutput)
-            val amount = (targetMaxInput / sum) * energy
-            if (amount > 0) {
-                if (source is MachineBlockEntity<*> && target is MachineBlockEntity<*>) {
-                    target.temperatureComponent?.inputOverflow = amount > target.getMaxInput(direction)
-                }
-                sourceHandler.into(targetHandler).move(amount)
-            }
-        }*/
     }
-
-    private fun isSideConnected(blockEntity: BlockEntity, direction: Direction) =
-        blockEntity !is CableBlockEntity || blockEntity.cachedState[CableBlock.getProperty(direction)]
 }
