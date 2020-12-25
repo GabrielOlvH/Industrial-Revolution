0b379bd7d4e69f8595129c0915583f3a36a3aa85
StupPlayer <gabrielh.oliveira222@gmail.com>
2020-12-25 17:15:56 -0300

    switch array map to open hash map

--- src/main/kotlin/me/steven/indrev/utils/EnergyApiUtils.kt
+++ src/main/kotlin/me/steven/indrev/utils/EnergyApiUtils.kt
@@ -4,17 +4,18 @@ import dev.technici4n.fasttransferlib.api.ContainerItemContext
 import dev.technici4n.fasttransferlib.api.energy.EnergyApi
 import dev.technici4n.fasttransferlib.api.energy.EnergyIo
 import dev.technici4n.fasttransferlib.api.item.ItemKey
-import it.unimi.dsi.fastutil.longs.Long2ObjectArrayMap
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap
 import net.fabricmc.fabric.api.lookup.v1.block.BlockApiCache
 import net.minecraft.item.ItemStack
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
+import java.util.function.LongFunction
 
-private val ENERGY_IO_CACHE = Long2ObjectArrayMap<BlockApiCache<EnergyIo, Direction>>()
+private val ENERGY_IO_CACHE = Long2ObjectOpenHashMap<BlockApiCache<EnergyIo, Direction>>()
 
 fun energyOf(world: ServerWorld, blockPos: BlockPos, direction: Direction): EnergyIo? {
-    return ENERGY_IO_CACHE.computeIfAbsent(blockPos.asLong()) { BlockApiCache.create(EnergyApi.SIDED, world, blockPos) }[direction]
+    return ENERGY_IO_CACHE.computeIfAbsent(blockPos.asLong(), LongFunction { BlockApiCache.create(EnergyApi.SIDED, world, blockPos) })[direction]
 }
 
 fun energyOf(itemStack: ItemStack?): EnergyIo? {
