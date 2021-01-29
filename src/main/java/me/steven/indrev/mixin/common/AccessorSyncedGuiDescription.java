45fa3b967f281252c0fb30b22f4d5052297518a0
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-01-29 13:09:21 -0300

    add remap = false in libgui mixins

--- src/main/java/me/steven/indrev/mixin/common/AccessorSyncedGuiDescription.java
+++ src/main/java/me/steven/indrev/mixin/common/AccessorSyncedGuiDescription.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-@Mixin(SyncedGuiDescription.class)
+@Mixin(value = SyncedGuiDescription.class, remap = false)
 public interface AccessorSyncedGuiDescription {
     @Invoker("insertItem")
     boolean indrev_callInsertItem(ItemStack toInsert, Inventory inventory, boolean walkBackwards, PlayerEntity player);
