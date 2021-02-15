243b4ef59cd9fcb2d744f1a1e308d1a826421d5d
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-02-15 10:57:50 -0300

    Fixed fluid input being extractable from Fluid Infuser

--- src/main/kotlin/me/steven/indrev/components/fluid/FluidInfuserFluidComponent.kt
+++ src/main/kotlin/me/steven/indrev/components/fluid/FluidInfuserFluidComponent.kt
@@ -12,37 +12,61 @@ import java.math.RoundingMode
 
 class FluidInfuserFluidComponent(machine: () -> MachineBlockEntity<*>) : FluidComponent(machine, FluidAmount.ofWhole(8) , 2) {
 
-    override fun getGroupedInv(): GroupedFluidInv {
-        return object : GroupedFluidInvFixedWrapper(this) {
+    private val grouped = object : GroupedFluidInvFixedWrapper(this) {
 
-            override fun attemptExtraction(
-                filter: FluidFilter?,
-                maxAmount: FluidAmount,
-                simulation: Simulation?
-            ): FluidVolume {
-                if (maxAmount.isNegative)
-                    throw IllegalArgumentException("maxAmount cannot be negative! (was $maxAmount)")
+        override fun attemptExtraction(
+            filter: FluidFilter?,
+            maxAmount: FluidAmount,
+            simulation: Simulation?
+        ): FluidVolume {
+            if (maxAmount.isNegative)
+                throw IllegalArgumentException("maxAmount cannot be negative! (was $maxAmount)")
 
-                var fluid = FluidVolumeUtil.EMPTY
-                if (maxAmount.isZero)
-                    return fluid
-
-                val thisMax = maxAmount.roundedSub(fluid.amount(), RoundingMode.DOWN)
-                fluid = extractFluid(1, filter, fluid, thisMax, simulation)
-                if (fluid.amount() >= maxAmount)
-                    return fluid
+            var fluid = FluidVolumeUtil.EMPTY
+            if (maxAmount.isZero)
                 return fluid
-            }
 
-            override fun attemptInsertion(immutableFluid: FluidVolume, simulation: Simulation?): FluidVolume {
-                var fluid = immutableFluid
-                if (fluid.isEmpty)
-                    return FluidVolumeUtil.EMPTY
-                fluid = insertFluid(0, fluid.copy(), simulation)
-                if (fluid.isEmpty)
-                    return FluidVolumeUtil.EMPTY
+            val thisMax = maxAmount.roundedSub(fluid.amount(), RoundingMode.DOWN)
+            fluid = extractFluid(1, filter, fluid, thisMax, simulation)
+            if (fluid.amount() >= maxAmount)
                 return fluid
-            }
+            return fluid
         }
+
+        override fun attemptInsertion(immutableFluid: FluidVolume, simulation: Simulation?): FluidVolume {
+            var fluid = immutableFluid
+            if (fluid.isEmpty)
+                return FluidVolumeUtil.EMPTY
+            fluid = insertFluid(0, fluid.copy(), simulation)
+            if (fluid.isEmpty)
+                return FluidVolumeUtil.EMPTY
+            return fluid
+        }
+    }
+
+    override fun getGroupedInv(): GroupedFluidInv = grouped
+
+    override fun attemptInsertion(fluid: FluidVolume, simulation: Simulation?): FluidVolume? {
+        return grouped.attemptInsertion(fluid, simulation)
+    }
+
+    override fun getMinimumAcceptedAmount(): FluidAmount? {
+        return grouped.minimumAcceptedAmount
+    }
+
+    override fun getInsertionFilter(): FluidFilter? {
+        return grouped.insertionFilter
+    }
+
+    override fun attemptExtraction(
+        filter: FluidFilter?,
+        maxAmount: FluidAmount,
+        simulation: Simulation?
+    ): FluidVolume {
+        return grouped.attemptExtraction(filter, maxAmount, simulation)
+    }
+
+    override fun attemptAnyExtraction(maxAmount: FluidAmount?, simulation: Simulation?): FluidVolume? {
+        return grouped.attemptAnyExtraction(maxAmount, simulation)
     }
 }
