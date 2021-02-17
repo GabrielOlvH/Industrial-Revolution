c72ccc7eb5a3d0d5d80dee3f3bf557b05af73b95
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-02-17 16:50:00 -0300

    ops :)

--- src/main/java/me/steven/indrev/mixin/client/MixinLivingEntityClient.java
+++ src/main/java/me/steven/indrev/mixin/client/MixinLivingEntityClient.java
@@ -4,6 +4,7 @@ import me.steven.indrev.api.IRPlayerEntityExtension;
 import me.steven.indrev.tools.modular.ArmorModule;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +14,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MixinLivingEntityClient {
     @Inject(method = "hasStatusEffect", at = @At("INVOKE"), cancellable = true)
     private void indrev_nightVision(StatusEffect effect, CallbackInfoReturnable<Boolean> cir) {
-        if (this instanceof IRPlayerEntityExtension && ((IRPlayerEntityExtension) this).isApplied(ArmorModule.NIGHT_VISION)) {
+        if (
+                effect.equals(StatusEffects.NIGHT_VISION)
+                && this instanceof IRPlayerEntityExtension
+                && ((IRPlayerEntityExtension) this).isApplied(ArmorModule.NIGHT_VISION)
+        ) {
             cir.setReturnValue(true);
         }
     }
