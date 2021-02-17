374b4b493622ba1e9012546fcba0fd7fea8c4240
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-02-17 17:20:23 -0300

    Port drain fixes by rMuchan to the pump

--- src/main/kotlin/me/steven/indrev/blockentities/farms/PumpBlockEntity.kt
+++ src/main/kotlin/me/steven/indrev/blockentities/farms/PumpBlockEntity.kt
@@ -1,6 +1,5 @@
 package me.steven.indrev.blockentities.farms
 
-import alexiil.mc.lib.attributes.Simulation
 import alexiil.mc.lib.attributes.fluid.amount.FluidAmount
 import alexiil.mc.lib.attributes.fluid.volume.FluidKeys
 import me.steven.indrev.api.machines.Tier
@@ -11,10 +10,14 @@ import me.steven.indrev.blocks.machine.HorizontalFacingMachineBlock
 import me.steven.indrev.components.fluid.FluidComponent
 import me.steven.indrev.config.BasicMachineConfig
 import me.steven.indrev.registry.MachineRegistry
-import me.steven.indrev.utils.map
+import me.steven.indrev.utils.contains
 import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable
 import net.minecraft.block.BlockState
+import net.minecraft.block.FluidBlock
 import net.minecraft.block.FluidDrainable
+import net.minecraft.fluid.FlowableFluid
+import net.minecraft.fluid.Fluid
+import net.minecraft.fluid.Fluids
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Box
@@ -35,50 +38,56 @@ class PumpBlockEntity(tier: Tier) : MachineBlockEntity<BasicMachineConfig>(tier,
     var isDescending = false
     var lastYPos = -1
 
-    var areaIterator = emptySet<BlockPos>().iterator()
-
     override fun machineTick() {
+        val fluidComponent = fluidComponent ?: return
         val currentLevel = floor(movingTicks).toInt()
         val lookLevel = pos.offset(Direction.DOWN, currentLevel)
-        if (!isDescending && ticks % config.processSpeed.toInt() == 0 && canUse(config.energyCost)) {
-            if (world?.getFluidState(lookLevel)?.isEmpty == true) {
-                lastYPos = lookLevel.y
-                isDescending = true
-                return
-            }
-            movingTicks = movingTicks.roundToInt().toDouble()
-            val fluidComponent = fluidComponent ?: return
-            val areaBox = getWorkingArea(lookLevel)
-            if (!areaIterator.hasNext())
-                areaIterator = areaBox.map(::BlockPos).sortedWith(compareByDescending { it.getSquaredDistance(pos) }).iterator()
-            while (areaIterator.hasNext()) {
-                val fluidPos = areaIterator.next()
-                val fluidState = world?.getFluidState(fluidPos)
-                val fluid = fluidState?.fluid
-                val block = fluidState?.blockState?.block
-                if (block is FluidDrainable && !fluidState.isEmpty && fluidState.isStill) {
-                    val toInsert = FluidKeys.get(fluid).withAmount(FluidAmount.BUCKET)
-                    if (fluidComponent.insertable.attemptInsertion(toInsert, Simulation.SIMULATE).isEmpty) {
-                        block.tryDrainFluid(world, fluidPos, fluidState.blockState)
-                        fluidComponent.insertable.insert(toInsert)
-                        use(config.energyCost)
+        val currentFluid = world?.getFluidState(lookLevel)
+        if (!isDescending && ticks % config.processSpeed.toInt() == 0 && canUse(config.energyCost) && fluidComponent[0].isEmpty) {
+            if (currentFluid?.isEmpty == false && canUse(config.energyCost)) {
+                val range = getWorkingArea(lookLevel)
+                val directions = arrayOf(Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST)
+                val mutablePos = pos.mutableCopy()
+                val fluid = getStill(currentFluid.fluid)
+                val bfs = mutableListOf(lookLevel)
+                var i = 0
+                while (i < bfs.size) {
+                    val current = bfs[i++]
+                    for (dir in directions) {
+                        mutablePos.set(current, dir)
+                        if (mutablePos !in bfs && mutablePos in range && getStill(world!!.getFluidState(mutablePos).fluid) === fluid) {
+                            bfs.add(mutablePos.toImmutable())
+                        }
                     }
-                    return
+                }
+
+                for (pos in bfs) {
+                    val blockState = world!!.getBlockState(pos)
+                    val block = blockState?.block
+                    if (block is FluidDrainable && block is FluidBlock) {
+                        val drained = block.tryDrainFluid(world, pos, blockState)
+                        if (drained != Fluids.EMPTY) {
+                            val toInsert = FluidKeys.get(drained).withAmount(FluidAmount.BUCKET)
+                            fluidComponent.insertable.insert(toInsert)
+                            use(config.energyCost)
+                            return
+                        }
+                    }
                 }
             }
             lastYPos = lookLevel.y
             isDescending = true
-            return
         }
-        else if (world?.getFluidState(lookLevel)?.isEmpty == false && lookLevel.y < lastYPos) {
+        else if (currentFluid?.isEmpty == false && lookLevel.y < lastYPos) {
             isDescending = false
-            areaIterator = getWorkingArea(lookLevel).map(::BlockPos).sortedWith(compareByDescending { it.getSquaredDistance(pos) }).iterator()
-        }
-        else if (use(2.0) && (lookLevel == pos || (world?.isAir(lookLevel) == true && world?.getFluidState(lookLevel)?.isEmpty != false))) {
+            movingTicks = movingTicks.roundToInt().toDouble()
+        } else if (use(2.0) && (lookLevel == pos || (world?.isAir(lookLevel) == true && currentFluid?.isEmpty != false))) {
             movingTicks += 0.01
             sync()
         }
     }
+
+    private fun getStill(fluid: Fluid): Fluid = if (fluid is FlowableFluid) fluid.still else fluid
 
     override fun applyDefault(
         state: BlockState,
