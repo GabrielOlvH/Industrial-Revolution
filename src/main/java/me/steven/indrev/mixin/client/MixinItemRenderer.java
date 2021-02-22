f615eeebff71be495e99b971a9e2f754f2f42ecf
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-02-21 21:42:55 -0300

    fix the mixin i made to fix another bug

--- src/main/java/me/steven/indrev/mixin/client/MixinItemRenderer.java
+++ src/main/java/me/steven/indrev/mixin/client/MixinItemRenderer.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
@@ -15,7 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  */
 @Mixin(value = ItemRenderer.class, priority = 1001)
 public class MixinItemRenderer {
-    @Inject(method = "renderGuiItemOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At("HEAD"), cancellable = true)
+    @Inject(
+            method = "renderGuiItemOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V",
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isDamaged()Z")),
+            at = @At("HEAD"),
+            cancellable = true
+    )
     private void a(TextRenderer renderer, ItemStack stack, int x, int y, String countLabel, CallbackInfo ci) {
         if (stack.getItem() instanceof IREnergyItem) {
             ci.cancel();
