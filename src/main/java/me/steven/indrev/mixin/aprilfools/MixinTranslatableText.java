483317f1052b1e843e802f9846a1fc9f874953ac
Guilherme Leoi <geeleonidas@gmail.com>
2021-03-22 20:47:55 -0300

    more owo

--- src/main/java/me/steven/indrev/mixin/aprilfools/MixinTranslatableText.java
+++ src/main/java/me/steven/indrev/mixin/aprilfools/MixinTranslatableText.java
@@ -6,26 +6,35 @@ import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import java.util.Locale;
 
 @Mixin(TranslatableText.class)
 public class MixinTranslatableText {
     @Shadow @Final private String key;
 
-    @ModifyVariable(
-        method = "visitSelf(Lnet/minecraft/text/StringVisitable$StyledVisitor;Lnet/minecraft/text/Style;)Ljava/util/Optional;",
-        name = "stringVisitable",
-        at = @At(value = "STORE")
-    )
-    private StringVisitable owo(StringVisitable original) {
+    @ModifyVariable(name = "string", at = @At("STORE"), method = "updateTranslations")
+    private String owo(String original) {
         if (this.key.contains(IndustrialRevolution.MOD_ID)) {
-            String str = original.getString();
-            if (!str.contains("."))
-                return StringVisitable.plain(
-                    str.replace("O", "Ow")
-                       .replace("o", "ow")
-                );
+            if (!original.contains(".")) {
+                String[] words = original.split(" ");
+                for (int i = 0; i < words.length; i++) {
+                    if (words[i].endsWith("er") || words[i].endsWith("re"))
+                        words[i] = words[i].substring(0, words[i].length() - 2) + 'y';
+                    else if (words[i].endsWith("xe"))
+                        words[i] = words[i].substring(0, words[i].length() - 2) + 'x';
+                    else if (words[i].endsWith("et"))
+                        words[i] = words[i].substring(0, words[i].length() - 2) + 'i';
+
+                    words[i] = words[i].replaceFirst("O", "Ow").replaceFirst("o", "ow");
+                }
+
+                return String.join(" ", words);
+            }
         }
 
         return original;
