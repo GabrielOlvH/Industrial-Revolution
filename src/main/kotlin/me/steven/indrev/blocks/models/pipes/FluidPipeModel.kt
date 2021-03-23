3fa05e50af4bf71637d7e6761e7882159d6501e1
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-03-23 17:52:00 -0300

    fluid pipe model

--- src/main/kotlin/me/steven/indrev/blocks/models/pipes/FluidPipeModel.kt
+++ src/main/kotlin/me/steven/indrev/blocks/models/pipes/FluidPipeModel.kt
@@ -1,14 +1,12 @@
 package me.steven.indrev.blocks.models.pipes
 
 import me.steven.indrev.api.machines.Tier
+import me.steven.indrev.utils.blockSpriteId
 import net.minecraft.client.util.SpriteIdentifier
 
 class FluidPipeModel(tier: Tier) : BasePipeModel(tier, "fluid_pipe") {
     override val spriteIdCollection: MutableList<SpriteIdentifier> = mutableListOf(
-        //this is an example, replace this with the textures from the item pipe
-        /*blockSpriteId("block/cable_center"),
-        blockSpriteId("block/cable_center_emissive_${tier.toString().toLowerCase()}"),
-        blockSpriteId("block/cable_wrap"),
-        blockSpriteId("block/cable_wire_emissive_${tier.toString().toLowerCase()}")*/
+        blockSpriteId("block/fluid_pipe_center_${tier.toString().toLowerCase()}"),
+        blockSpriteId("block/fluid_pipe_side_${tier.toString().toLowerCase()}")
     )
 }
