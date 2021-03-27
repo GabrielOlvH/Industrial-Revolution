0b4504e986ccaf5dff5a2c45afdcbc012d2174ae
Guilherme Leoi <geeleonidas@gmail.com>
2021-03-27 20:01:57 -0300

    noubowdy saw dat ;);)

--- src/main/java/me/steven/indrev/AprilFools.java
+++ src/main/java/me/steven/indrev/AprilFools.java
@@ -6,8 +6,8 @@ import java.io.File;
 import java.time.LocalDate;
 
 public class AprilFools {
-    public static final LocalDate DATE = LocalDate.parse("2021-03-27");
-    public static final File CHECK = new File(FabricLoader.INSTANCE.getGameDir().toString() + ".indrev_ANO");
+    public static final LocalDate DATE = LocalDate.parse("2021-04-01");
+    public static final File CHECK = new File(FabricLoader.INSTANCE.getGameDir().toString() + ".indrev_2021");
 
     public static boolean isToday() {
         return LocalDate.now().equals(DATE) && !CHECK.exists();
