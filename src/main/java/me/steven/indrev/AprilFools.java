1f0c07bc4a2b3f09be59c114b5d7ac7ca0886e59
Guilherme Leoi <geeleonidas@gmail.com>
2021-03-28 13:37:39 -0300

    sowy mista stiven i did it again...

--- src/main/java/me/steven/indrev/AprilFools.java
+++ src/main/java/me/steven/indrev/AprilFools.java
@@ -6,10 +6,11 @@ import java.io.File;
 import java.time.LocalDate;
 
 public class AprilFools {
-    public static final LocalDate DATE = LocalDate.parse("2021-04-01");
-    public static final File CHECK = new File(FabricLoader.INSTANCE.getGameDir().toString() + ".indrev_2021");
+    public static final File CHECK =
+        new File(FabricLoader.INSTANCE.getGameDir() + "/.indrev_" + LocalDate.now().getYear());
 
     public static boolean isToday() {
-        return LocalDate.now().equals(DATE) && !CHECK.exists();
+        LocalDate now = LocalDate.now();
+        return now.getDayOfMonth() == 1 && now.getMonthValue() == 4 && !CHECK.exists();
     }
 }
