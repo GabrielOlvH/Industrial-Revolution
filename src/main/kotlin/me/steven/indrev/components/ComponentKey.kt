2dd4e0a70b3a417bcc703e8e17f974a84c82ee01
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-06-17 22:35:10 -0300

    use componentkeys instead of hardcoded MachineBlockEntity

--- src/main/kotlin/me/steven/indrev/components/ComponentKey.kt
+++ src/main/kotlin/me/steven/indrev/components/ComponentKey.kt
@@ -4,11 +4,20 @@ import io.github.cottonmc.cotton.gui.PropertyDelegateHolder
 import me.steven.indrev.components.fluid.FluidComponent
 import me.steven.indrev.components.multiblock.MultiBlockComponent
 import me.steven.indrev.utils.identifier
+import net.minecraft.block.entity.BlockEntity
 import net.minecraft.util.Identifier
 
+/**
+ * Used for some internals, allows to provide components without spamming interfaces
+ * for example: a Solar Power Plant Smelter has a TemperatureComponent however it is not a child of MachineBlockEntity.
+ */
 class ComponentKey<T>(val id: Identifier) {
 
+    @Suppress("UNCHECKED_CAST")
     fun get(provider: ComponentProvider): T? = provider.get(this) as T?
+
+    @Suppress("UNCHECKED_CAST")
+    fun get(provider: BlockEntity): T? = (provider as? ComponentProvider ?: error("$provider is not a ComponentProvider")).get(this) as T?
 
     companion object {
         val FLUID = ComponentKey<FluidComponent>(identifier("fluid"))
