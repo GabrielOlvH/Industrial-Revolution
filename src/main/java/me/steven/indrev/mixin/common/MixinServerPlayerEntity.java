daa5a63c44d0e03e7d9c4d36dbd1488c1e9e6640
GabrielOlvH <gabrielh.oliveira222@gmail.com>
2021-09-20 17:12:01 -0300

    don't auto eat always edible food (closes #293)

--- src/main/java/me/steven/indrev/mixin/common/MixinServerPlayerEntity.java
+++ src/main/java/me/steven/indrev/mixin/common/MixinServerPlayerEntity.java
@@ -135,7 +135,7 @@ public abstract class MixinServerPlayerEntity extends PlayerEntity implements IR
                                 for (int slot = 0; slot <= inventory.size(); slot++) {
                                     ItemStack stack = inventory.getStack(slot);
                                     FoodComponent food = stack.getItem().getFoodComponent();
-                                    if (food != null && !HelperextensionsKt.hasNegativeEffects(food) && food.getHunger() <= 20 - hunger.getFoodLevel() && EnergyutilsKt.extract(itemStack, 30.0)) {
+                                    if (food != null && !food.isAlwaysEdible() && !HelperextensionsKt.hasNegativeEffects(food) && food.getHunger() <= 20 - hunger.getFoodLevel() && EnergyutilsKt.extract(itemStack, 30.0)) {
                                         stack.finishUsing(world, player);
                                         player.eatFood(world, stack);
                                     }
