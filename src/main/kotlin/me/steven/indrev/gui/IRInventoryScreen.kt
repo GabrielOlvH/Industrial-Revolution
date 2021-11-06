548fd09fbb9273573d7e02ade073e92c86194854
Plamen K. Kosseff <p.kosseff@gmail.com>
2021-11-06 12:49:40 +0200

    Adds interoperability with Inventory Profiles Next.

--- src/main/kotlin/me/steven/indrev/gui/IRInventoryScreen.kt
+++ src/main/kotlin/me/steven/indrev/gui/IRInventoryScreen.kt
@@ -4,6 +4,8 @@ import io.github.cottonmc.cotton.gui.SyncedGuiDescription
 import io.github.cottonmc.cotton.gui.client.CottonInventoryScreen
 import io.github.cottonmc.cotton.gui.impl.client.MouseInputHandler
 import net.minecraft.entity.player.PlayerEntity
+import org.anti_ad.mc.ipn.api.IPNIgnore
 
+@IPNIgnore
 class IRInventoryScreen<T : SyncedGuiDescription>(controller: T, playerEntity: PlayerEntity) : CottonInventoryScreen<T>(controller, playerEntity) {
 }
